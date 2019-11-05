371cf3d196f573cdfeff98b4060069f58e68909d
Parvathy Menon <parvathy.menon1997@gmail.com>
2019-11-04 19:27:45 -0800

    Adding updated gitignore

--- src/api/utils/keys.py
+++ src/api/utils/keys.py
@@ -2,5 +2,5 @@ class Keys:
     API_KEY = 'dn1j4olzNIiHc9SWJmYhRHLR1ytzLQrVc-B0P-kcPzECXQaisAknSQeq70Gnxj2DoLhWwfTnN1YNWDA89bRWbtogs_qvN_gpK8qvvZqPelpOWiUCUg6UZE0SkatsXXYx'
     API_HOST = 'https://api.yelp.com'
     BUSINESS_PATH = '/v3/businesses/'
-    PKL_DIR = '/Users/parvathy/Desktop/senior-project/svdpp_las_vegas_existing_user_model.pkl'
-    CSV_DIR = "/Users/parvathy/Desktop/senior-project/us_restaurant_review_lasvegas_nv.csv"
+    PKL_DIR = '/Users/parvathy/Desktop/senior-project/src/svdpp_las_vegas_existing_user_model.pkl'
+    CSV_DIR = "/Users/parvathy/Desktop/senior-project/src/us_restaurant_review_lasvegas_nv.csv"
