71d73cdad040e93da1f2639d340540d8924ea760
Parvathy Menon <parvathy.menon1997@gmail.com>
2019-11-05 17:11:11 -0800

    Added secure path

--- front-end/client/src/app/layout/App.jsx
+++ front-end/client/src/app/layout/App.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import NavBar from '../../features/nav/NavBar/NavBar';
+import React, { Component } from "react";
+import NavBar from "../../features/nav/NavBar/NavBar";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
-import ProfileDashboard from '../../features/Profile/ProfileDashboard';
-import Test from '../../features/Test';
-import Testing from '../../components/ItemList';
-import ExistingUser from '../../features/yelpuser/ExistingUser/ExistingUser'
-import store from '../store/store';
-import { loadUser } from '../actions/authActions';
+import ProfileDashboard from "../../features/Profile/ProfileDashboard";
+import Test from "../../features/Test";
+import Testing from "../../components/ItemList";
+import ExistingUser from "../../features/yelpuser/ExistingUser/ExistingUser";
+import store from "../store/store";
+import { loadUser } from "../actions/authActions";
+import withAuth from "../../components/auth/withauth";
 
 class App extends Component {
   componentDidMount() {
@@ -20,13 +21,21 @@ class App extends Component {
         <div>
           <NavBar />
           <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route exact path='/testing' component={Testing} />
-            <Route exact path='/home' component={HomePage} />
-            <Route exact path='/test' component={Test} />
-            <Route exact path='/profile' component={ProfileDashboard} />
-            <Route exact path='/existinguser' component={ExistingUser} />
-            <Route path='*' component={() => "404 NOT FOUND"} />
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/testing" component={Testing} />
+            <Route exact path="/home" component={HomePage} />
+            <Route exact path="/test" component={Test} />
+            <Route
+              exact
+              path="/profile"
+              component={withAuth(ProfileDashboard)}
+            />
+            <Route
+              exact
+              path="/existinguser"
+              component={withAuth(ExistingUser)}
+            />
+            <Route path="*" component={() => "404 NOT FOUND"} />
           </Switch>
         </div>
       </Router>
